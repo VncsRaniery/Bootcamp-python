2f918c18d21ba47335d4326064555cd45ac7a752
Vinícius Raniery <152623369+VncsRaniery@users.noreply.github.com>
2024-04-09 22:08:41 -0300

    Adição de novas anotações e programas em python e organização em pastas dos conteúdos

---
+++ Python Essencial/variaveis_Contastantes.py
@@ -0,0 +1,12 @@
+nome = "Guilherme"
+idade = 25
+
+nome, idade = "João", 56
+
+print(nome, idade)
+
+limite_saque_diáro = 10000
+
+BRAZILIAN_STATES = ["São Paulo", "Rio de Janeiro", "Minas Gerais"]
+
+print(BRAZILIAN_STATES)
