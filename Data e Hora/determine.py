e6d3754f3fd8c9fd70af3e567fa4114acf4e9bd9
Vinícius Raniery <152623369+VncsRaniery@users.noreply.github.com>
2024-05-07 23:38:58 -0300

    introdução do conteúdo data e hora utilizando a linguagem python

---
+++ Data e Hora/determine.py
@@ -0,0 +1,12 @@
+from datetime import date, datetime, time
+
+data = date(2020, 12, 25)
+print(data)
+print(data.today())
+
+data_hora = date(2020, 12, 25)
+print(data_hora)
+print(data_hora.today())
+
+hora = time(10, 20, 0)
+print(hora)
